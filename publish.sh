453a2066efa0b46fdd7e957c5c548decf63c4bad
Fernando Serboncini <fserb@fserb.com.br>
2014-04-11 00:41:20 +0200

    Publish for Flash and HTML5

--- publish.sh
+++ publish.sh
@@ -8,11 +8,18 @@ for f in src/*; do
   ./select.py "$f"
   D=`cat "$f" | grep -v "//" | grep Game.debug`
   if [ "$D" == "" ]; then
+    N=`echo "$f" | cut -c 5- | rev | cut -c 4- | rev`
+    echo "$N"
+    mkdir -p "$TARGET/$N"
     lime build flash
     if [ $? == 0 ]; then
-      N=`echo "$f" | cut -c 5- | rev | cut -c 4- | rev`
-      echo "$N"
-      cp bin/flash/bin/$N.swf "$TARGET"
+      cp bin/flash/bin/$N.swf "$TARGET/$N/flash.swf"
+    fi
+    lime build html5
+    if [ $? == 0 ]; then
+      cp bin/html5/bin/index.html "$TARGET/$N/"
+      uglifyjs bin/html5/bin/$N.js -c -m > "$TARGET/$N/$N.js" 2>/dev/null
+      cp bin/html5/bin/howler.min.js "$TARGET/$N/"
     fi
   else
     N=`echo "$f" | cut -c 5- | rev | cut -c 4- | rev`
