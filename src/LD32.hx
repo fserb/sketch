d1d54ac3364589741fedebe4a9ea9ef270839c9e
Fernando Serboncini <fserb@fserb.com.br>
2015-04-18 16:59:41 -0400

    Attacks

--- src/LD32.hx
+++ src/LD32.hx
@@ -1,9 +1,8 @@
-//@ ugl.bgcolor = 0xffc704
+//@ ugl.bgcolor = 0x010101
 
 /*
 An Unconventional Weapon
 ========================
-
 
 
 */
@@ -20,19 +19,43 @@ class C {
   static public var black = 0x010101;
   static public var white = 0xFAFAFA;
   static public var yellow = 0xffc704;
-  static public var purple = 0x8c07a8;
-  static public var cyan = 0x03939a;
+  static public var purple = 0xc459dc;
+  static public var cyan = 0x3eb3b9;
 }
 
 class LD32 extends Micro {
+  var bg: BG;
+  var player: Player;
+  var counter: Float;
+  public var speed: Float;
   static public function main() {
     Micro.baseColor = C.black;
     new LD32("This game is called Grab", "");
   }  
 
   override public function begin() {
-    new Player(); 
-    new Minion();
+    bg = new BG();
+    player = new Player(); 
+    new Minion(C.yellow);
+    new Minion(C.cyan);
+    bg.change(C.purple);
+    counter = 3.5;
+    speed = 1.0;
+  }
+
+  public function change(color: UInt) {
+    // EATEN
+    counter = 3.5;
+    new Minion(Game.scene.bg.color);
+    Game.shake(0.2);
+    bg.change(color);
+    speed *= 1.1;
+  }
+
+  override public function update() {
+    counter += Game.time;
+    if (bg.color == C.cyan) {
+    }
   }
 }
 
@@ -43,6 +66,7 @@ class Player extends Entity {
     pos.x = pos.y = 240;
     gfx.fill(C.black).circle(16, 16, 16);
     hook = new Hook(this);
+    addHitBox(Circle(16,16,16));
   }
 
   override public function update() {
@@ -62,6 +86,11 @@ class Player extends Entity {
       acc.length = vel.length = 0;
     }
 
+    if (pos.x < 0) pos.x += 480;
+    if (pos.x >= 480) pos.x -= 480;
+    if (pos.y < 0) pos.y += 480;
+    if (pos.y >= 480) pos.y -= 480;
+
   }
 }
 
@@ -77,7 +106,7 @@ class Hook extends Entity {
     arm = 0.0;
     action = 0;
     draw();
-    addHitBox(Rect(0, 0, 20, 14));
+    addHitBox(Rect(0, -4, 20, 18));
   }
 
   function draw() {
@@ -122,6 +151,9 @@ class Hook extends Entity {
         target = m;
         m.grabbed = true;
         action = 3;
+        for (b in Game.get("Bullet")) {
+          b.remove();
+        }
       }
 
       if (arm == maxarm) {
@@ -144,7 +176,7 @@ class Hook extends Entity {
 
       draw();
       if (arm == 0) {
-        // EATEN
+        Game.scene.change(target.color);
         action = 0;
       }
     }
@@ -154,28 +186,141 @@ class Hook extends Entity {
   }
 }
 
-class Minion extends Entity {
-  static var layer = 10;
-  public var type = 1;
-  public var grabbed = false;
+class Bullet extends Entity {
+  public var color: UInt;
   override public function begin() {
-    pos.x = 100;
-    pos.y = 100;
-    type = 2;
-    var color = switch(type) {
-      case 1: C.purple;
-      case 2: C.cyan;
-      case 3: C.yellow;
-      default: C.black;
-    };
-
-    gfx.fill(color).circle(10,10,10).fill(color).rect(0, 10, 20, 10);
-    gfx.fill(C.white, 0.9).circle(7,8,2).circle(13,8,2);
-    // gfx.fill(C.black, 1.0).circle(7,8,1).circle(13,8,1);
-    addHitBox(Rect(0, 0, 20, 20));
+    color = args[0];
+    gfx.fill(color).circle(8, 8, 8);
+    addHitBox(Circle(8,8,8));
   }
 
   override public function update() {
+    if (color == C.purple) {
 
+    } else if (color == C.cyan) {
+
+    } else {
+      if (pos.x < 0 || pos.x > 480 || pos.y < 0 || pos.y > 480) remove();
+    }
+  }
+}
+
+class Minion extends Entity {
+  static var layer = 10;
+  public var color:UInt;
+  public var grabbed = false;
+  var target: Vec2;
+  var bullet: Bullet;
+  var bulletAngle = 0.0;
+  var bulletDirection = false;
+  override public function begin() {
+    pos.x = 480*Math.random();
+    pos.y = 480*Math.random();
+    color = args[0];
+    gfx.fill(color).circle(10,10,10).fill(color).rect(0, 10, 20, 10);
+    gfx.fill(C.white, 0.9).circle(7,8,2).circle(13,8,2);
+    addHitBox(Rect(0, 0, 20, 20));
+
+    target = new Vec2(pos.x, pos.y);
+  }
+
+  override public function update() {
+    if (color == Game.scene.bg.color) {
+      this.remove();
+      if (bullet != null) bullet.remove();
+    }
+    if (grabbed) {
+      vel.x = vel.y = 0;
+    } else {
+
+      if (target.x < 0) target.x += 480;
+      if (target.x >= 480) target.x -= 480;
+      if (target.y < 0) target.y += 480;
+      if (target.y >= 480) target.y -= 480;
+
+      if (color == C.purple) {
+        if (bullet == null || bullet.dead) {
+        bullet = new Bullet(color);
+        bulletDirection = Math.random() < 0.5;
+        }
+
+        target = Game.scene.player.pos.distance(pos);
+        if (target.length < 128) {
+          target.length = -128;
+        } else {
+          target.length = 128;
+        }
+        target.add(pos);
+
+        var dt = target.distance(pos);
+        dt.clamp(Game.scene.speed*50*Game.time);
+        pos.add(dt);
+
+        bulletAngle += (bulletDirection ? 1 : -1)*Math.PI*Game.time*0.1*Game.scene.speed;
+        bullet.pos.x = pos.x + 128*Math.cos(bulletAngle);
+        bullet.pos.y = pos.y + 128*Math.sin(bulletAngle);
+
+      } else if (color == C.cyan) {
+        var dt = target.distance(pos);
+        if (dt.length < 10) {
+          if (bullet != null) bullet.remove();
+          bullet = new Bullet(color);
+
+          target = Game.scene.player.pos.distance(pos);
+          target.length = 200;
+          target.add(pos);
+          bullet.pos = pos.copy();
+        } else {
+          var db = target.distance(bullet.pos);
+          if (db.length > 5) {
+            db.clamp(Game.scene.speed*100*Game.time);
+            bullet.pos.add(db);
+          } else {
+            dt.clamp(Game.scene.speed*50*Game.time);
+            pos.add(dt);
+          }
+        }
+      } else {
+        var c = new Vec2(240, 240);
+        c.sub(Game.scene.player.pos);
+        c.length = 200;
+        c.x += 240;
+        c.y += 240;
+        target = c;
+        var dt = target.distance(pos);
+        dt.clamp(Game.scene.speed*50*Game.time);
+        pos.add(dt);
+
+        if (dt.length < 10) {
+          if (bullet == null || bullet.dead) {
+            bullet = new Bullet(color);
+            bullet.pos = pos.copy();
+            bullet.vel = Game.scene.player.pos.distance(pos);
+            bullet.vel.length = Game.scene.speed*100;
+          }
+        }
+      }
+      if (pos.y < 0) pos.x += 480;
+      if (pos.x >= 480) pos.x -= 480;
+      if (pos.y < 0) pos.y += 480;
+      if (pos.y >= 480) pos.y -= 480;
+    }
+  }
+}
+
+class BG extends Entity {
+  static var layer = 1;
+  public var color: UInt;
+  override public function begin() {
+    pos.x = pos.y = 240;
+  }
+
+  public function change(color: UInt) {
+    gfx.clear();
+    this.color = color;
+    gfx.fill(color).rect(0, 0, 480, 480);
+  }
+
+  override public function update() {
   }
 }
