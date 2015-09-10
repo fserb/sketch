3e50372b08d3a218a4c8692267c38d1c11a6e95d
Fernando Serboncini <fserb@fserb.com.br>
2015-09-09 21:39:47 -0400

    Grow fail

--- src/Grow.hx
+++ src/Grow.hx
@@ -17,122 +17,218 @@ class Grow extends Micro {
     new Cursor();
   }
 
-
   override public function update() {
+  }
+}
+
+class Node {
+  public var pos: Vec2;
+  public var normal: Vec2;
+  public var next: Node;
+  public function new(x: Float, y: Float, next: Node) {
+    pos = new Vec2(x, y);
+    this.next = next;
+    normal = null;
+  }
+
+  public function frac(f: Float): Vec2 {
+    var p2 = next.pos.distance(pos);
+    p2.mul(f - Std.int(f));
+    var p = pos.copy();
+    p.add(p2);
+    return p;
   }
 }
 
 class Path extends Entity {
   var zoom: Float = 1.0;
-  var points: Array<Float>;
-  var added: Array<Float>;
+
+  var points: Node;
+  var added: Node;
   var addedStart: Float;
   var length: Float;
+
   override public function begin() {
     pos.x = pos.y = 0;
     alignment = TOPLEFT;
-    points = new Array<Float>();
-    added = new Array<Float>();
+    points = new Node(240 + 100, 240, null);
+    points.next = points;
+    added = null;
     addedStart = -1;
-    for (i in 0...100) {
-      points.push(100);
-      added.push(100);
+    var p: Node = points;
+    for (i in 1...10) {
+      p.next = new Node(240 + 100*Math.cos(2*Math.PI*i/10),
+        240 + 100*Math.sin(2*Math.PI*i/10), points);
+      p = p.next;
     }
+    calcLength();
   }
 
-  inline function getidx(a: Float): Float {
-    var ang = a%(2*Math.PI);
-    return points.length*ang/(2*Math.PI);
+  function calcLength() {
+    var p = points;
+    trace("length");
+    do {
+      var d = p.next.pos.distance(p.pos).length;
+      if (d < 10) {
+        if (p.next == points) {
+          points = p.next.next;
+        }
+        p.next = p.next.next;
+        continue;
+      }
+
+      p = p.next;
+    } while (p != points);
+    trace("skip");
+    length = 0.0;
+    var p = points;
+    var prev: Node = points;
+    while (prev.next == points) prev = prev.next;
+    do {
+      length += p.next.pos.distance(p.pos).length;
+      p.normal = prev.pos.distance(p.next.pos).normal();
+      p.normal.normalize();
+      prev = p;
+      p = p.next;
+    } while (p != points);
   }
 
-  function get(a: Float): Vec2 {
-    var idx = getidx(a);
-    var b = Std.int(idx);
-    var p0 = new Vec2(240 + zoom*points[b]*Math.cos(2*Math.PI*b/points.length),
-                      240 + zoom*points[b]*Math.sin(2*Math.PI*b/points.length));
-    var e = (b + 1) % points.length;
-    var p1 = new Vec2(240 + zoom*points[e]*Math.cos(2*Math.PI*e/points.length),
-                      240 + zoom*points[e]*Math.sin(2*Math.PI*e/points.length));
-    p1.sub(p0);
-    p1.mul(idx - b);
-    p0.add(p1);
-    return p0;
+  function get(t: Float): Vec2 {
+    var l = 0.0;
+    t = t % length;
+    var p = points;
+    do {
+      var sl = p.next.pos.distance(p.pos).length;
+      l += sl;
+      if (l > t) {
+        return p.frac((t-(l-sl))/sl);
+      }
+      p = p.next;
+    } while (p != points);
+    return new Vec2(0, 0);
   }
 
   public function normal(t: Float): Vec2 {
-    var p = get(t).copy();
-    p.x -= 240;
-    p.y -= 240;
-    p.normalize();
-    return p;
+    var l = 0.0;
+    t = t % length;
+    var p = points;
+    do {
+      var sl = p.next.pos.distance(p.pos).length;
+      l += sl;
+      if (l > t) {
+        l -= sl;
+        var r1 = p.normal.copy();
+        var r2 = p.next.normal.distance(r1);
+        r2.mul((t-l)/sl);
+        r1.add(r2);
+        return r1 ;
+      }
+      p = p.next;
+    } while (p != points);
+    return new Vec2(0, 0);
   }
 
-  public function addPath(a: Float, h: Float) {
-    var idx = getidx(a);
-    var a = Std.int(idx);
-    added[a] = points[a] + h;
-    var b = (a+1)%points.length;
-    added[b] = points[b] + h;
-  }
-
-  public function closePath(t: Float) {
-    for (i in 0...added.length) {
-      points[i] = added[i];
+  public function addPath(t: Float, p: Vec2) {
+    if (addedStart < 0) {
+      addedStart = t;
     }
+    if (added == null) {
+      added = new Node(p.x, p.y, null);
+      added.next = added;
+    } else {
+      var a = added;
+      while (a.next != added) a = a.next;
+      a.next = new Node(p.x, p.y, added);
+    }
+  }
+
+  public function closePath(addedEnd: Float): Float {
+    if (added == null) return addedEnd;
+    if (addedEnd < addedStart) {
+      var t = addedEnd;
+      addedEnd = addedStart;
+      addedStart = t;
+    }
+
+    var l = 0.0;
+    var tstart = addedStart % length;
+    var tend = addedEnd % length;
+    var stepstart = 0.0;
+    var stepend = 0.0;
+    var p = points;
+    var startNode: Node = null;
+    var endNode: Node = null;
+    do {
+      var sl = p.next.pos.distance(p.pos).length;
+      l += sl;
+      if (startNode == null && l > tstart) {
+        stepstart = (tstart-(l-sl))/sl;
+        startNode = p;
+      }
+      if (endNode == null && l > tend) {
+        stepend = (tend-(l-sl))/sl;
+        endNode = p;
+      }
+      p = p.next;
+    } while (p != points);
+    // add half begin point.
+    var r1 = startNode.frac(stepstart);
+    startNode.next = new Node(r1.x, r1.y, startNode.next);
+
+    // add half end point.
+    var r1 = endNode.frac(stepend);
+    endNode.next = new Node(r1.x, r1.y, endNode.next);
+
+    // remove middle points.
+    var la = added;
+    while (la.next != added) la = la.next;
+    startNode.next.next = added;
+    la.next = endNode.next;
+
+    added = null;
+    addedStart = -1;
+    points = endNode.next;
+
+    calcLength();
+    return 0;
   }
 
   public function draw() {
     gfx.clear();
-    gfx.line(2, 0x888888);
-    var last = -1;
-    for (i in 0...added.length) {
-      if (added[i] != points[i]) {
-        var x = 240 + zoom*added[i]*Math.cos(2*Math.PI*i/added.length);
-        var y = 240 + zoom*added[i]*Math.sin(2*Math.PI*i/added.length);
-        if (last == -1) {
-          gfx.mt(x, y);
-        }
-        gfx.lt(x, y);
-        last = i;
-      } else if (last >= 0) {
-        gfx.mt(240 + zoom*added[last]*Math.cos(2*Math.PI*last/added.length),
-               240 + zoom*added[last]*Math.sin(2*Math.PI*last/added.length));
-        last = -1;
 
-      }
-    }
-    if (added[0] != points[0]) {
-      gfx.lt(240 + zoom*added[0], 240);
-      last = 0;
-    }
-    if (last >= 0) {
-        gfx.mt(240 + zoom*added[last]*Math.cos(2*Math.PI*last/added.length),
-               240 + zoom*added[last]*Math.sin(2*Math.PI*last/added.length));
+    if (added != null) {
+      gfx.line(2, 0x888888);
+      var p = added;
+      gfx.mt(added.pos.x, added.pos.y);
+      do {
+        gfx.lt(p.pos.x, p.pos.y);
+        p = p.next;
+      } while (p != added);
+      gfx.mt(p.pos.x, p.pos.y);
     }
 
-    gfx.line(2, 0xFFFFFF).mt(240 + zoom*points[0], 240);
-    for (i in 0...points.length) {
-      var h = points[i];
-      gfx.lt(240 + zoom*h*Math.cos(2*Math.PI*i/points.length),
-             240 + zoom*h*Math.sin(2*Math.PI*i/points.length));
-    }
-    gfx.lt(240 + zoom*points[0], 240);
+    gfx.line(2, 0xFFFFFF);
+    var p = points;
+    gfx.mt(points.pos.x, points.pos.y);
+    do {
+      gfx.lt(p.pos.x, p.pos.y);
+      p = p.next;
+    } while (p != points);
+    gfx.lt(points.pos.x, points.pos.y);
+
+    p = points;
+    do {
+      gfx.circle(p.pos.x, p.pos.y, 3);
+      p = p.next;
+    } while (p != points);
   }
 
   override public function update() {
     draw();
-    var mv = 0.0;
-    for (p in points) {
-      if (p > mv) mv = p;
-    }
-    for (p in added) {
-      if (p > mv) mv = p;
-    }
+    var mv = 1.0;
     var targetzoom = Math.min(1.0, 175.0/mv);
     var dz = targetzoom - zoom;
-    trace(zoom, targetzoom);
     zoom += dz*Game.time;
-
   }
 }
 
@@ -140,24 +236,34 @@ class Cursor extends Entity {
   var t: Float;
   var h: Float;
   var hv: Float;
+  var normal: Vec2;
+  var direction: Float;
+
   override public function begin() {
     gfx.fill(0xFFFFFF).circle(8, 8, 8);
     t = 0.0;
     h = 0.0;
     hv = 0.0;
+    normal = null;
+    direction = 1.0;
   }
 
   override public function update() {
     pos = Game.one("Path").get(t);
-    var n:Vec2 = Game.one("Path").normal(t);
 
     var ha = 0.0;
 
     var C1 = 0.1;
     var C2 = 10.0;
 
-    if (Game.key.b1) {
+    if (Game.key.b1 || Game.mouse.button) {
       ha = 500.0;
+      normal = Game.one("Path").normal(t);
+    }
+
+    if (normal == null) {
+      t += Game.time*50*direction;
+      return;
     }
 
     if (hv > 0) {
@@ -167,26 +273,29 @@ class Cursor extends Entity {
     }
     ha -= h*C2;
 
-
     ha *= Game.time;
     h += Game.time*(hv + ha/2.0);
     hv += ha;
 
     if (h < 0) h =0.0;
 
-    var n2 = n.copy();
+    var n2 = normal.copy();
     n2.mul(h);
     pos.add(n2);
 
     gfx.clear();
     gfx.fill(0xFFFFFF).circle(8, 8, 8);
-    gfx.fill().line(2, 0x000000).mt(8,8).lt(8 + 8*n.x, 8 + 8*n.y);
+    gfx.fill().line(2, 0x000000).mt(8,8).lt(8 + 8*normal.x, 8 + 8*normal.y);
 
     if (h > 0) {
-      Game.one("Path").addPath(t, h);
+      Game.one("Path").addPath(t, pos);
     } else {
-      Game.one("Path").closePath(t);
+      t = Game.one("Path").closePath(t);
+      if (normal != null) {
+        normal = null;
+        direction = -direction;
+      }
     }
-    t += Game.time*0.75;
+    t += Game.time*50*direction;
   }
 }
